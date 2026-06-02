dc3d9132651ffb6a8038a86159842ede30206165
ATHTHANAYAKE A M S V <IT23762336@my.sliit.lk>
2026-06-02 15:50:00 +0530

    feat: implement admin dashboard overview page

---
+++ admin/src/pages/Orders.jsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { backendUrl, currency } from "../App.jsx";
+import { toast } from "react-toastify";
+import { assets } from "../assets/assets.js";
+
+const Orders = ({ token }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    if (!token) {
+      return null;
+    }
+    try {
+      const response = await axios.post(
+        backendUrl + "api/order/list",
+        {},
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        console.log(response.data);
+        setOrders(response.data.orders);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const statusHandler = async (event, orderId)=>{
+    try{
+      const response = await axios.post(backendUrl+'api/order/status',{orderId,status:event.target.value},{headers:{token}})
+      if(response.data.success){
+        await fetchAllOrders()
+      }
+    }catch(error){
+      console.log(error)
+      toast.error(response.data.message)
+    }
+
+  }
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [token]);
+
+  return (
+    <div>
+      <h3>Order Page</h3>
+      <div>
+        {orders.map((order, index) => (
+          <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700' key={index}>
+            <img className="w-12" src={assets.parcel_icon} alt="" />
+            <div>
+              <div>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return (
+                      <p className='py-0.5' key={index}>
+                        {item.name} x {item.quantity} <span> {item.size} </span>{" "}
+                      </p>
+                    );
+                  } else {
+                    return (
+                      <p className='py-0.5' key={index}>
+                        {item.name} x {item.quantity} <span> {item.size} </span>{" "}
+                        ,
+                      </p>
+                    );
+                  }
+                })}
+              </div>
+              <p className='mt-3 mb-2 font-medium' >
+                {order.address[0].firstName + " " + order.address[0].lastName}
+              </p>
+              <div>
+                <p>{order.address[0].street + " ,"}</p>
+                <p>
+                  {order.address[0].city +
+                    " , " +
+                    order.address[0].state +
+                    " , " +
+                    order.address[0].country +
+                    " , " +
+                    order.address[0].zipcode}
+                </p>
+              </div>
+              <p>{order.address[0].phone}</p>
+            </div>
+            <div>
+              <p className='text-sm sm:text-[15px]' >Items : {order.items.length}</p>
+              <p className='mt-3' >Method : {order.paymentMethod}</p>
+              <p>Payment: {order.payment === 'true' ? 'Done' : 'Pending'}</p>
+              <p>Date: {order.date}</p>
+
+            </div>
+            <p className='text-sm sm:text-[15px]' >{currency} {order.amount}</p>
+            <select onChange={(event)=> statusHandler(event,order._id)} value={order.status} className='p-2 font-semibold ' >
+             <option value="Order Placed">Order Placed</option>
+              <option value="Ready to Ship">Ready to Ship</option>
+              <option value="Shipped">Shipped</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
